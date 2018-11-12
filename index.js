462b81598219c3cb3757a0bc691f478f9d234c18
joetakara <44985154+joetakara@users.noreply.github.com>
2018-11-13 05:19:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Dimensions } from 'react-native';
 
-export const vw = percentageWidth => Dimensions.get('window').width * (percentageWidth / 100);
-export const vh = percentageHeight => Dimensions.get('window').height * (percentageHeight / 100);
-export const vmin = percentage => Math.min(Dimensions.get('window').width * (percentage / 100), Dimensions.get('window').height * (percentage / 100));
-export const vmax = percentage => Math.max(Dimensions.get('window').width * (percentage / 100), Dimensions.get('window').height * (percentage / 100));
+export const vw = number => Dimensions.get('window').width * (number / 100);
+export const vh = number => Dimensions.get('window').height * (number / 100);
+export const vmin = number => Math.min(Dimensions.get('window').width * (number / 100), Dimensions.get('window').height * (number / 100));
+export const vmax = number => Math.max(Dimensions.get('window').width * (number / 100), Dimensions.get('window').height * (number / 100));
