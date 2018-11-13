d9f3c4ad528ab45a49ea286a6e66759f875521b4
joetakara <44985154+joetakara@users.noreply.github.com>
2018-11-13 20:07:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { Dimensions } from 'react-native';
-
 export const vw = number => Dimensions.get('window').width * (number / 100);
 export const vh = number => Dimensions.get('window').height * (number / 100);
 export const vmin = number => Math.min(Dimensions.get('window').width * (number / 100), Dimensions.get('window').height * (number / 100));
